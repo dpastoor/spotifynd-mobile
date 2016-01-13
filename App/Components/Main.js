2432f6d7581d1de1d5bd30bf6a47d262b5aec4ca
Devin Pastoor <devin.pastoor@gmail.com>
2016-01-12 19:07:13 -0800

    add signin

--- App/Components/Main.js
+++ App/Components/Main.js
@@ -2,11 +2,15 @@
  * Created by devin on 1/12/16.
  */
 var React = require('react-native');
+var config = require('../config');
 
 var {
   View,
   Text,
-  StyleSheet
+  StyleSheet,
+  TextInput, // text input
+  TouchableHighlight, //capture touch event and do something with it
+  ActivityIndicatorIOS // show a spinner based on a boolean
   } = React;
 
 var styles = StyleSheet.create({
@@ -54,10 +58,58 @@ var styles = StyleSheet.create({
 });
 
 class Main extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      isLoading: false,
+      error: false
+    }
+  }
+
+  handleChange(event) {
+    this.setState({
+      username: event.nativeEvent.text
+    });
+  }
+
+  handleSubmit(event) {
+     // underlayColor --> any time press on button background goes white
+    // eventually will fetch data from 4square
+    //reroute to next passing in information
+    // for now will just fake
+    this.setState({
+      isLoading: true
+    });
+    //directly hitting the 4square api
+    //fetch('https://api.foursquare.com/v2/venues/explore?client_id='+
+    //  config.API+
+    //  '&client_secret='+config.SECRET+'&v=20130815&near='+this.state.username+'&venuePhotos=1')
+    
+      .then(function(res) {
+      console.log('resolved:')
+        console.log(res)
+        console.log(JSON.parse(res._bodyText));
+    })
+    // once loaded turn back off to false
+    console.log("SUBMIT: " + this.state.username)
+  }
   render() {
     return(
       <View style={styles.mainContainer}>
-        <Text> Testing the Router </Text>
+        <Text style={styles.title}> Search for a Place </Text>
+        <TextInput
+          style={styles.searchInput}
+          value={this.state.username}
+          onChange={this.handleChange.bind(this)}
+        />
+        <TouchableHighlight
+          style={styles.button}
+          onPress={this.handleSubmit.bind(this)}
+          underlayColor="white"
+        >
+          <Text style={styles.buttonText}> SEARCH</Text>
+        </TouchableHighlight>
       </View>
     )
   }
